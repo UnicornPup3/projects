ce3f66d8d642103080eb42452f73cf475e5d5cb4
Megan Bergamo <megan@Senna.local>
2022-06-22 21:43:31 -0700

    Project 20 / 22

---
+++ PRO-C20-V3-Template-main/PlayerArcher.js
@@ -0,0 +1,4 @@
+this.body = Matter.Bodies(x,y,width,height,options);
+this.body = Matter.Bodies.rect(x,y,width,height);
+this.body = Matter.rectangle(x,y,width,hight,options);
+this.body = Matter.Bodies.rectangle(x,y,width,height,options);
